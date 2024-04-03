ad78adf51ddd77b60afe31c189b84af48d700f83
Saurabh Mishra <msourabh200@gmail.com>
2024-04-03 10:04:59 +0530

    Reverse Linked List

---
+++ reverseLinkedList.go
@@ -0,0 +1,13 @@
+func reverseList(head *ListNode) *ListNode {
+    var prev, next *ListNode
+	cur := head
+
+	for cur != nil {
+		next = cur.Next  
+		cur.Next = prev  
+		prev = cur       
+		cur = next      
+	}
+
+	return prev 
+}
